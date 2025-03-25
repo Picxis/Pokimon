447351ede90b874343e04cda88ab33ec11682ba2
Picxis <jdrodriguezd28@gmail.com>
2025-03-25 16:38:15 -0500

    Registro

---
+++ Js/Registro.js
@@ -0,0 +1,13 @@
+function registrarse() {
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirmPassword').value;
+
+    if (password !== confirmPassword) {
+        alert("Las contraseñas no coinciden. Intenta nuevamente.");
+    }
+
+    alert('Registrado correctamente');
+}
