0b3c25e5a019a98093949cf681d73b051ed566aa
David <jdrodriguezd28@gmail.com>
2025-04-07 22:17:46 -0500

    Arreglos

--- Js/entrenador.js
+++ Js/entrenador.js
@@ -11,24 +11,5 @@ function DatosUser() {
 
     // Convertimos los datos a formato JSON
     let user = JSON.parse(dataUser);
-
-    // Obtenemos los datos a mostar en la pagina mediante el ID
-    let Entrenador = document.getElementById("idEntrenador");
-    let nombre = document.getElementById("nombre");
-    let apellido = document.getElementById("apellido");
-
-    //Si el dato existe, lo actualizamos en la pagina, el ID
-    if (Entrenador) {
-        Entrenador.innerHTML = "ID: " + user.nombreEntrenador;
-    }
-    //Si el dato existe, lo actualizamos en la pagina, el nombre
-    if (nombre) {
-        nombre.innerHTML = user.nombre;
-    }
-    //Si el dato existe, lo actualizamos en la pagina, el apellido
-    if (apellido) {
-        apellido.innerHTML = user.apellido;
-    }
 }
-// Llamamos la funcion para ejecutarla
 
